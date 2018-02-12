0cdd3f5ac0db41ecad3526bb7908b368aeab99cd
Jack Crowson <crowstar@localhost.localdomain>
2018-02-12 18:41:44 +0000

    remove dropout for generator, add genre splitter script

--- model.py
+++ model.py
@@ -354,19 +354,19 @@ class DCGAN(object):
 
         self.h0 = tf.reshape(
             self.z_, [-1, s_h16, s_w16, self.gf_dim * 8])
-        h0 = tf.nn.dropout(tf.nn.relu(self.g_bn0(self.h0)), 0.4)
+        h0 = tf.nn.relu(self.g_bn0(self.h0))
 
         self.h1, self.h1_w, self.h1_b = deconv2d(
             h0, [self.batch_size, s_h8, s_w8, self.gf_dim*4], name='g_h1', with_w=True)
-        h1 = tf.nn.dropout(tf.nn.relu(self.g_bn1(self.h1)), 0.4)
+        h1 = tf.nn.relu(self.g_bn1(self.h1))
 
         h2, self.h2_w, self.h2_b = deconv2d(
             h1, [self.batch_size, s_h4, s_w4, self.gf_dim*2], name='g_h2', with_w=True)
-        h2 = tf.nn.dropout(tf.nn.relu(self.g_bn2(h2)), 0.4)
+        h2 = tf.nn.relu(self.g_bn2(h2))
 
         h3, self.h3_w, self.h3_b = deconv2d(
             h2, [self.batch_size, s_h2, s_w2, self.gf_dim*1], name='g_h3', with_w=True)
-        h3 = tf.nn.dropout(tf.nn.relu(self.g_bn3(h3)), 0.4)
+        h3 = tf.nn.relu(self.g_bn3(h3))
 
         h4, self.h4_w, self.h4_b = deconv2d(
             h3, [self.batch_size, s_h, s_w, self.c_dim], name='g_h4', with_w=True)
@@ -381,18 +381,18 @@ class DCGAN(object):
         yb = tf.reshape(y, [self.batch_size, 1, 1, self.y_dim])
         z = concat([z, y], 1)
 
-        h0 = tf.nn.dropout(tf.nn.relu(
-            self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin'))), 0.4)
+        h0 = tf.nn.relu(
+            self.g_bn0(linear(z, self.gfc_dim, 'g_h0_lin')))
         h0 = concat([h0, y], 1)
 
-        h1 = tf.nn.dropout(tf.nn.relu(self.g_bn1(
-            linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin'))), 0.4)
+        h1 = (tf.nn.relu(self.g_bn1(
+            linear(h0, self.gf_dim*2*s_h4*s_w4, 'g_h1_lin')))
         h1 = tf.reshape(h1, [self.batch_size, s_h4, s_w4, self.gf_dim * 2])
 
         h1 = conv_cond_concat(h1, yb)
 
-        h2 = tf.nn.dropout(tf.nn.relu(self.g_bn2(deconv2d(h1,
-            [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2'))), 0.4)
+        h2 = tf.nn.relu(self.g_bn2(deconv2d(h1,
+            [self.batch_size, s_h2, s_w2, self.gf_dim * 2], name='g_h2')))
         h2 = conv_cond_concat(h2, yb)
 
         return tf.nn.tanh(
---
+++ sort_by_genre.py
@@ -0,0 +1,44 @@
+import os
+import shutil
+import csv
+
+
+verbose = True
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+# recursively remove everything in genres folder
+dest_folder = 'genres'
+if os.path.isdir(dest_folder):
+	shutil.rmtree(dest_folder)
+# create a new empty genre folder
+os.mkdir(dest_folder) 
+
+# read csv file into memory
+reader = csv.DictReader(open('train_info.csv'))
+train_info = list(reader)
+
+# iterate through each item in paintings list
+count = 0
+for item in train_info:
+
+	count += 1
+
+	filename = item['filename']
+	genre = item['genre']
+	src_path = current_dir + '/train/' + filename
+	dest_path = current_dir + '/genres/' + genre + '/' + filename
+
+	# create a new folder for new genre
+	if not (os.path.isdir(current_dir + '/genres/' + genre)):
+		os.mkdir(current_dir + '/genres/' + genre) 
+
+	# make copy of file from train folder and put it into its genre folder
+	if os.path.exists(src_path) and genre != '':
+		shutil.copyfile(src_path, dest_path)
+
+	if verbose:
+		if (count % 1000 == 0):
+			print('Progress:' + str(count))
+
+
