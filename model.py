3fcd2a9559e66781e05337660669db988f949947
Jack Crowson <crowstar@localhost.localdomain>
2018-03-10 15:40:10 +0000

    fix typo

--- model.py
+++ model.py
@@ -77,77 +77,77 @@ class DCGAN(object):
       self.data_X, self.data_y = self.load_labelled_data()
       imreadImg = imread(self.data_X[0])
       if len(imreadImg.shape) >= 3: #check if image is a non-grayscale image by checking channel number
-        self.c_dim = imread(self.data[0]).shape[-1]
-      else:
-        self.c_dim = 1
+            self.c_dim = imread(self.data_X[0]).shape[-1]
+          else:
+            self.c_dim = 1
 
-    self.grayscale = (self.c_dim == 1)
+        self.grayscale = (self.c_dim == 1)
 
-    self.build_model()
+        self.build_model()
 
-  def build_model(self):
-    if self.y_dim:
-      self.y = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='y')
-    else:
-      self.y = None
+      def build_model(self):
+        if self.y_dim:
+          self.y = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='y')
+        else:
+          self.y = None
 
-    if self.crop:
-      image_dims = [self.output_height, self.output_width, self.c_dim]
-    else:
-      image_dims = [self.input_height, self.input_width, self.c_dim]
+        if self.crop:
+          image_dims = [self.output_height, self.output_width, self.c_dim]
+        else:
+          image_dims = [self.input_height, self.input_width, self.c_dim]
 
-    self.inputs = tf.placeholder(
-      tf.float32, [self.batch_size] + image_dims, name='real_images')
+        self.inputs = tf.placeholder(
+          tf.float32, [self.batch_size] + image_dims, name='real_images')
 
-    inputs = self.inputs
+        inputs = self.inputs
 
-    self.z = tf.placeholder(
-      tf.float32, [None, self.z_dim], name='z')
-    self.z_sum = histogram_summary("z", self.z)
+        self.z = tf.placeholder(
+          tf.float32, [None, self.z_dim], name='z')
+        self.z_sum = histogram_summary("z", self.z)
 
-    self.G                  = self.generator(self.z, self.y)
-    self.D, self.D_logits   = self.discriminator(inputs, self.y, reuse=False)
-    self.sampler            = self.sampler(self.z, self.y)
-    self.D_, self.D_logits_ = self.discriminator(self.G, self.y, reuse=True)
-    
-    self.d_sum = histogram_summary("d", self.D)
-    self.d__sum = histogram_summary("d_", self.D_)
-    self.G_sum = image_summary("G", self.G)
+        self.G                  = self.generator(self.z, self.y)
+        self.D, self.D_logits   = self.discriminator(inputs, self.y, reuse=False)
+        self.sampler            = self.sampler(self.z, self.y)
+        self.D_, self.D_logits_ = self.discriminator(self.G, self.y, reuse=True)
+        
+        self.d_sum = histogram_summary("d", self.D)
+        self.d__sum = histogram_summary("d_", self.D_)
+        self.G_sum = image_summary("G", self.G)
 
-    def sigmoid_cross_entropy_with_logits(x, y):
-      try:
-        return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, labels=y)
-      except:
-        return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, targets=y)
+        def sigmoid_cross_entropy_with_logits(x, y):
+          try:
+            return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, labels=y)
+          except:
+            return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, targets=y)
 
-    self.d_loss_real = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
-    self.d_loss_fake = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
-    self.g_loss = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
+        self.d_loss_real = tf.reduce_mean(
+          sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
+        self.d_loss_fake = tf.reduce_mean(
+          sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
+        self.g_loss = tf.reduce_mean(
+          sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
 
-    self.d_loss_real_sum = scalar_summary("d_loss_real", self.d_loss_real)
-    self.d_loss_fake_sum = scalar_summary("d_loss_fake", self.d_loss_fake)
-                          
-    self.d_loss = self.d_loss_real + self.d_loss_fake
+        self.d_loss_real_sum = scalar_summary("d_loss_real", self.d_loss_real)
+        self.d_loss_fake_sum = scalar_summary("d_loss_fake", self.d_loss_fake)
+                              
+        self.d_loss = self.d_loss_real + self.d_loss_fake
 
-    self.g_loss_sum = scalar_summary("g_loss", self.g_loss)
-    self.d_loss_sum = scalar_summary("d_loss", self.d_loss)
+        self.g_loss_sum = scalar_summary("g_loss", self.g_loss)
+        self.d_loss_sum = scalar_summary("d_loss", self.d_loss)
 
-    t_vars = tf.trainable_variables()
+        t_vars = tf.trainable_variables()
 
-    self.d_vars = [var for var in t_vars if 'd_' in var.name]
-    self.g_vars = [var for var in t_vars if 'g_' in var.name]
+        self.d_vars = [var for var in t_vars if 'd_' in var.name]
+        self.g_vars = [var for var in t_vars if 'g_' in var.name]
 
-    self.saver = tf.train.Saver()
+        self.saver = tf.train.Saver()
 
-  def train(self, config):
-    d_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
-              .minimize(self.d_loss, var_list=self.d_vars)
-    g_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
-              .minimize(self.g_loss, var_list=self.g_vars)
-    try:
+      def train(self, config):
+        d_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
+                  .minimize(self.d_loss, var_list=self.d_vars)
+        g_optim = tf.train.AdamOptimizer(config.learning_rate, beta1=config.beta1) \
+                  .minimize(self.g_loss, var_list=self.g_vars)
+        try:
       tf.global_variables_initializer().run()
     except:
       tf.initialize_all_variables().run()
@@ -194,16 +194,16 @@ class DCGAN(object):
       if config.dataset == 'mnist':
         batch_idxs = min(len(self.data_X), config.train_size) // config.batch_size
       else:      
-        self.data = glob(os.path.join(
+        self.data_X = glob(os.path.join(
           "./data", config.dataset, self.input_fname_pattern))
-        batch_idxs = min(len(self.data), config.train_size) // config.batch_size
+        batch_idxs = min(len(self.data_X), config.train_size) // config.batch_size
 
       for idx in xrange(0, batch_idxs):
         if config.dataset == 'mnist':
           batch_images = self.data_X[idx*config.batch_size:(idx+1)*config.batch_size]
         
         else:
-          batch_files = self.data[idx*config.batch_size:(idx+1)*config.batch_size]
+          batch_files = self.data_X[idx*config.batch_size:(idx+1)*config.batch_size]
           batch = [
               get_image(batch_file,
                         input_height=self.input_height,
